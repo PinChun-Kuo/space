12a93176f68fbeddb5af782187399a66d5a217f5
PinChun-Kuo <pinery201@gmail.com>
2020-11-17 17:02:09 +0800

    Scaffolding

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const temp = (<div>Hello Space</div>);
+
+ReactDOM.render(
+  temp,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['./src/index.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env']
+        }
+      }
+    }, {
+      test: /\.(scss|sass)$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  devServer: {
+    port: 3000
+  }
+};
